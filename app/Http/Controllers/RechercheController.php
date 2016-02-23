eee41bddaeb202f014e79aa049dfce85aad2aedb
Julie Ramadanoski <ramadanoski.j@gmail.com>
2016-02-23 18:03:24 +0100

    elaboration requete securisee

--- app/Http/Controllers/RechercheController.php
+++ app/Http/Controllers/RechercheController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Trajet;
+use DateTime;
 
 class RechercheController extends Controller
 {
@@ -17,10 +18,13 @@ class RechercheController extends Controller
      */
     public function show(Request $request)
     {
-        $trajets = Trajet::TrajetsTrouves();
-        
-       dd($request);
 
+        //dd($request->input('datedepart')); "02/17/2016 12:00 AM"
+        $date = DateTime::createFromFormat('m/d/Y H:i A', $request->input('datedepart') );
+        $dateRecherche = $date->format('Y-m-d');
+        
+        $trajets = Trajet::TrajetsTrouves( $dateRecherche, $request->input('villedepart'),  $request->input('villearrivee'),  $request->input('vehicule'));
+        
         return view('recherche.resultats', compact('trajets'));
     }
 }
--- app/Trajet.php
+++ app/Trajet.php
@@ -1,5 +1,6 @@
 <?php namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Trajet extends Model {
@@ -37,9 +38,48 @@ class Trajet extends Model {
         return $this->hasMany(\App\Question::class, 'idTraj', 'idTraj');
     }
 
-    public function scopeTrajetsTrouves(){
+    public function scopeTrajetsTrouves( $queryTraj, $dateRecherche, $villeDepart, $villeArrivee, $vehicule){
         // ecrire la requete pour trouver les trajets correspondants à une demande
-        return 'ci git la liste des trajets';
+        
+        // SAFE QUERY http://fideloper.com/laravel-raw-queries
+        /*$someVariable = Input::get("some_variable");
+
+        $results = DB::select( DB::raw("SELECT * FROM some_table WHERE some_col = :somevariable"), array(
+           'somevariable' => $someVariable,
+         ));*/
+        //dd($dateRecherche);
+       //dd($villeDepart);
+         dd($queryTraj);
+/*
+        $rowdepart = DB::table('ville')->where('nomVille', $villeDepart )->get();
+       
+       
+        $query = "select *
+        from etapeTrajet as et2
+        where et2.idTraj in(
+            select et.idTraj
+            from etapeTrajet as et
+            natural join etape as e
+            natural join trajet as t
+            natural join ville as v
+            -- where t.dateTraj = '2016-01-17'
+            -- coord de bourg saint andéol 44.3667, 4.65 < 5km de pierrelatte 44.9333, 4.9
+            where get_distance_kms(v.latitudeVille, v.longitudeVille, 44.3667, 4.65) <= t.detoursTraj
+            -- coord de portes les valence 44.8667, 4.8833 < 10km de valence
+            -- and get_distance_kms(v.latitudeVille, v.longitudeVille, 44.8667, 4.8833) <= t.detoursTraj
+            and et.idTraj in (
+                -- selectionne ville d'arrivée sur le meme trajet
+                select et1.idTraj
+                from etapeTrajet as et1
+                natural join etape as e1
+                natural join ville as v1
+                where v1.nomVille = 'aix-en-provence'
+                -- ou la ville d'arrivé sera déservie apres la ville de départ
+                and et1.numOrdreEtapeTrajet > et.numOrdreEtapeTrajet))";
+
+        $results = DB::select( DB::raw($query) );
+
+        return $results;        */
 
     }
 
