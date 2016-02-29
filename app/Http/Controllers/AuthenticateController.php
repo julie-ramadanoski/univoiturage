5e5a48e040bc25221682600947d2317d632f0e12
Julie Ramadanoski <ramadanoski.j@gmail.com>
2016-02-29 22:54:45 +0100

    Ajout des fichiers manquants

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use JWTAuth;
+use App\User;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthenticateController extends Controller
+{
+
+    public function __construct()
+   {
+       // Apply the jwt.auth middleware to all methods in this controller
+       // except for the authenticate method. We don't want to prevent
+       // the user from retrieving their token if they don't already have it
+       $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+   }
+
+	public function index()
+	{
+	    // Retrieve all the users in the database and return them
+	    $users = User::all();
+	    return $users;
+	}
+
+    public function authenticate(Request $request)
+    {
+        // grab credentials from the request
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(compact('token'));
+    }
+}
