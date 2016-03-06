1b8e1f4b06aa331fea3f324399d221397a831010
Julie Ramadanoski <ramadanoski.j@gmail.com>
2016-03-07 00:25:44 +0100

    Focntionnement de l'authentification avec laravel branche mobile

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -17,6 +17,7 @@ class AuthenticateController extends Controller
        // except for the authenticate method. We don't want to prevent
        // the user from retrieving their token if they don't already have it
        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+       $this->middleware('cors');
    }
 
 	public function index()
