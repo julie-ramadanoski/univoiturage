7ad345a99ae45a59b8561787d17d6a164eeaf9e2
Julie Ramadanoski <ramadanoski.j@gmail.com>
2016-03-14 12:20:56 +0100

    modif

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -27,12 +27,34 @@ class AuthenticateController extends Controller
     }
 
     
-    public function getAlertes()
+    public function getAlertes(Request $request)
     {
-         // the token is valid and we have found the user via the sub claim
+        
+        try {
+           
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
+        /* // the token is valid and we have found the user via the sub claim
           if( $this->getAuthenticatedUser() ){
             return Alerte::all();
-          }
+          }*/
     }
     
     public function authenticate(Request $request)
