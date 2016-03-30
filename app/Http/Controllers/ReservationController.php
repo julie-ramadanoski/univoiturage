b1b790df711709374233c131a4753f2dae403a8d
Florian Tonnelier <fl.tonnelier@gmail.com>
2016-03-30 15:43:51 +0200

    ajout controller

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\MessageBag;
+
+use Validator;
+
+use App\Trajet;
+use DateTime;
+
+class ReservationController extends Controller
+{
+    public function show(Request $request)
+    {
+        $columnSizes = [
+                  'sm' => [4, 8],
+                  'lg' => [2, 10]
+                ];
+
+        return view('reservation', compact('columnSizes')); 
+    }
+
+    public function update(Request $request){
+        $user = Auth::user();
+        $user->name = $request->input('name');
+        $user->prenomMemb = $request->input('prenomMemb');
+        $user->email = $request->input('email');
+        $user->telMobMemb = $request->input('telMobMemb');
+        $user->anNaisMemb = $request->input('anNaisMemb');
+        $user->prefMemb = $request->input('prefMemb');
+        $user->photoMemb = $request->input('urlphoto');
+        // $user->password = bcrypt(bcrypt($request->input['password']));
+        $user->save();
+        // dd($user);
+        
+        $columnSizes = [
+                  'sm' => [4, 8],
+                  'lg' => [2, 10]
+                ];
+
+        return view('profil.form', compact('columnSizes')); 
+    }
+}
